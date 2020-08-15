21dbe78c673499ce4c764d6df11a5cfef36e7c46
Akshay Barpute <abarpute66@gmail.com>
2020-08-15 21:30:24 +0530

    Layout code changes

--- components/Layout.js
+++ components/Layout.js
@@ -1,41 +1,27 @@
-import Header from '../components/Header';
+// import Header from '../components/Header';
 import { Provider } from 'react-redux';
-import store from '../store/index';
+// import store from '../store/index';
 import Head from  'next/head';
-import {Component} from 'react';
-import { initGA, logPageView } from '../others/Analytics';
+// import { initGA, logPageView } from '../others/Analytics';
 
 
 
-const title = 'freetests4u.com';
+// const title = 'freetests4u.com';
 
-export default class Layout extends Component{
 
-    constructor(props){
-        super(props)
-    }
 
-    componentDidMount () {
-        if (!window.GA_INITIALIZED) {
-          initGA()
-          window.GA_INITIALIZED = true
-        }
-        logPageView()
-      }
-
-      render () {
-    return (
-        <div >
-            <Provider store={store}>
-                <Head>
-                <title>{this.props.title}</title>
-                <meta name='description' content={this.props.desc}/>
-                    </Head>
-                <Header />
-                {this.props.children}
-            </Provider>
+const Layout=(props)=>{
+    return(
+        <div>
+            <Head>
+                <title>{props.title}</title>
+                <meta name='description' content={props.desc}/>
+            </Head>
+                {/* <Header /> */}
+            {props.children}
+            {/* </Provider> */} 
         </div>
     )
 }
-    
-}
+
+export default Layout;
